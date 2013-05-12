ea4fe5b2a81d9ab1f1759bbe5181196970e39384
Jesse Andrews <anotherjesse@gmail.com>
2013-05-11 17:31:05 -0700

    starting to make a simple ember app work

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -2,12 +2,39 @@ App = Ember.Application.create({
 	'rootElement': '#ember-app'
 });
 
-App.Router.map(function() {
-  // put your routes here
+App.Message = Ember.Object.extend({
+	author: null,
+	text: null
 });
 
+App.Message.reopenClass({
+	collection: [],
+	all: function() {
+		return this.collection;
+	},
+	add: function(author, text) {
+		var msg = App.Message.create({
+			author: author,
+			text: text
+		});
+
+		this.collection.pushObject(msg);
+	}
+})
+
 App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
-});
+	model: function() {
+		return App.Message.all();
+	}
+})
+
+// App.Router.map(function() {
+//   // put your routes here
+// });
+
+App.IndexController = Ember.ArrayController.extend({
+	send: function() {
+		App.Message.add("you", this.get("newMessage"));
+		this.set('newMessage', '');
+	}
+})
